709e09a6a081db1ad1e1feaee337064c0768f03f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-01-04 21:13:39 +0400

    Renamed atom-word-count to atom-hello-package

--- spec/atom-hello-package-spec.js
+++ spec/atom-hello-package-spec.js
@@ -1,6 +1,6 @@
 'use babel';
 
-import AtomWordCount from '../lib/atom-word-count';
+import AtomWordCount from '../lib/atom-hello-package';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -12,32 +12,32 @@ describe('AtomWordCount', () => {
 
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('atom-word-count');
+    activationPromise = atom.packages.activatePackage('atom-hello-package');
   });
 
-  describe('when the atom-word-count:toggle event is triggered', () => {
+  describe('when the atom-hello-package:toggle event is triggered', () => {
     it('hides and shows the modal panel', () => {
       // Before the activation event the view is not on the DOM, and no panel
       // has been created
-      expect(workspaceElement.querySelector('.atom-word-count')).not.toExist();
+      expect(workspaceElement.querySelector('.atom-hello-package')).not.toExist();
 
       // This is an activation event, triggering it will cause the package to be
       // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-word-count:toggle');
+      atom.commands.dispatch(workspaceElement, 'atom-hello-package:toggle');
 
       waitsForPromise(() => {
         return activationPromise;
       });
 
       runs(() => {
-        expect(workspaceElement.querySelector('.atom-word-count')).toExist();
+        expect(workspaceElement.querySelector('.atom-hello-package')).toExist();
 
-        let atomWordCountElement = workspaceElement.querySelector('.atom-word-count');
+        let atomWordCountElement = workspaceElement.querySelector('.atom-hello-package');
         expect(atomWordCountElement).toExist();
 
         let atomWordCountPanel = atom.workspace.panelForItem(atomWordCountElement);
         expect(atomWordCountPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'atom-word-count:toggle');
+        atom.commands.dispatch(workspaceElement, 'atom-hello-package:toggle');
         expect(atomWordCountPanel.isVisible()).toBe(false);
       });
     });
@@ -49,18 +49,18 @@ describe('AtomWordCount', () => {
       // workspaceElement to the DOM are generally slower than those off DOM.
       jasmine.attachToDOM(workspaceElement);
 
-      expect(workspaceElement.querySelector('.atom-word-count')).not.toExist();
+      expect(workspaceElement.querySelector('.atom-hello-package')).not.toExist();
 
       // This is an activation event, triggering it causes the package to be activated.
-      atom.commands.dispatch(workspaceElement, 'atom-word-count:toggle');
+      atom.commands.dispatch(workspaceElement, 'atom-hello-package:toggle');
 
       waitsForPromise(() => { return activationPromise; });
 
       runs(() => {
         // Now we can test for view visibility
-        let atomWordCountElement = workspaceElement.querySelector('.atom-word-count');
+        let atomWordCountElement = workspaceElement.querySelector('.atom-hello-package');
         expect(atomWordCountElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'atom-word-count:toggle');
+        atom.commands.dispatch(workspaceElement, 'atom-hello-package:toggle');
         expect(atomWordCountElement).not.toBeVisible();
       });
     });
--- spec/atom-hello-package-view-spec.js
+++ spec/atom-hello-package-view-spec.js
@@ -1,6 +1,6 @@
 'use babel';
 
-import AtomWordCountView from '../lib/atom-word-count-view';
+import AtomWordCountView from '../lib/atom-hello-package-view';
 
 describe('AtomWordCountView', () => {
   it('has one valid test', () => {
